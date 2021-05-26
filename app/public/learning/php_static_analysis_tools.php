e28bebb5de734456d486a490837a4cd85b28c28a
Danack <Danack@basereality.com>
2021-05-26 16:10:40 +0100

    Use more exact line.

--- app/public/learning/php_static_analysis_tools.php
+++ app/public/learning/php_static_analysis_tools.php
@@ -18,7 +18,7 @@ $fn = function (
 
     $contentLinks = getLearningContentLinks();
 
-    $editInfo = createPHPOpenDocsEditInfo('Edit page', __FILE__, null);
+    $editInfo = createPHPOpenDocsEditInfo('Edit page', __FILE__, __LINE__ - 5);
     $editInfo->addNameWithLink(
         "Edit content",
         "https://raw.githubusercontent.com/exakat/php-static-analysis-tools/master/README.md"
