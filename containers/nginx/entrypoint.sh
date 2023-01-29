99fca420fea624e7c9e055bec317809efc20854a
Danack <Danack@basereality.com>
2023-01-29 21:40:09 +0000

    Not currently caring.

--- containers/nginx/entrypoint.sh
+++ containers/nginx/entrypoint.sh
@@ -7,6 +7,8 @@ ENV_TO_USE=${ENV_DESCRIPTION:=default}
 
 echo "ENV_TO_USE is ${ENV_TO_USE}";
 
+git config --global --add safe.directory /var/app
+
 # Generate nginx config file for the centos,dev environment
 php vendor/bin/configurate \
     -p config.source.php \
