bfcb7d9248ff3a1c4158dcfbfe7494aac2036314
Danack <Danack@basereality.com>
2023-05-15 17:56:22 +0100

    Add date to update script.

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,11 +9,13 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
+# put current date as yyyy-mm-dd HH:MM:SS in $date
+printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1
 
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+    echo "Up-to-date ${date}"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    echo "Need to pull ${date}"
     git pull
     chown -R deployer:deployer *
     sh runProd.sh
